3ba07a642f138b3e900e0ad0756cbb80f4cc06e9
Johnny Wan <wonjohnny@outlook.com>
2020-03-09 19:39:26 -0700

    fixed auto fetch script, ignore all .log files

--- regular_pull.py
+++ regular_pull.py
@@ -1,10 +1,14 @@
 #! /usr/bin/python3
 import os
+import time
 
 while True:
     os.system('bash git_fetch_pull.sh >> git_logs.log')
-    os.system('printf "END OF ONE UPDATE \n\n" >> git_logs.log')
+    
+    localtime = time.asctime(time.localtime(time.time()))
+    os.system('printf "CURRENT TIME:' + localtime + ', END OF ONE UPDATE" >> git_logs.log')
+
     print('updated')
-    sleep(1800)
+    time.sleep(1800)
 
 exit()
